912dc3123c5fc341e8aa3e5c4ddf6c59af7df4ca
Stan Lemon <stanlemon@users.noreply.github.com>
2020-05-27 21:36:30 -0400

    Switch to hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "react-hot-loader";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import Database from "./util/Database";
 
